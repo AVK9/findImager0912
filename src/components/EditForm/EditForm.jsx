fa12b5331c39573db6568af4b81c26e0db078fec
AlexKat <katsubo79@gmail.com>
2023-12-23 01:28:40 +0200

    2+--

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -3,8 +3,10 @@ import { MdOutlineCancel } from 'react-icons/md';
 
 import { SearchFormStyled, FormBtn, InputSearch } from 'components';
 import { BtnEdit } from './EditForm.styled';
-
-export const EditForm = ({ onUpdate, onCancel, onChange, currentTodo }) => {
+// {
+//   onUpdate, onCancel, onChange, currentTodo;
+// }
+export const EditForm = () => {
   return (
     <SearchFormStyled>
       <BtnEdit type="button">
@@ -19,7 +21,7 @@ export const EditForm = ({ onUpdate, onCancel, onChange, currentTodo }) => {
         placeholder="EDIT TODO"
         name="edit"
         required
-        defaultValue={currentTodo.text}
+        // defaultValue={currentTodo.text}
         autoFocus
       />
     </SearchFormStyled>
